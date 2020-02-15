2575c93f1ae75d7c372eb09e5059fa29c5c906f3
aLosada7 <aldc30sc@gmail.com>
2020-02-15 19:37:28 +0100

    ole ole ole

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ const pool = new Pool({
   user: "qohevxsvamoqpe",
   host: "ec2-54-247-125-38.eu-west-1.compute.amazonaws.com",
   database: "d3rs5pp0o7bsfi",
-  password: "1234",
-  port: "c936e205038f8b2feceae596b3cc32c1c60dca5a8f34107a9242450af36fa435"
+  password: "c936e205038f8b2feceae596b3cc32c1c60dca5a8f34107a9242450af36fa435",
+  port: "5432"
 });
 
 app.get('/*', function(req,res) {
     pool.query(
-        "INSERT INTO public.log(id, edad, sexo, tiempocountries, tiempoquiz, aciertos, fallos, tiempo) VALUES ('prueba','1','',14,14,14,14,'');",
+        "INSERT INTO public.log(id, edad, sexo, tiempocountries, tiempoquiz, aciertos, fallos, tiempo, pistasrestantes) VALUES ('prueba','1','',14,14,14,14,'',2);",
         (err, res) => {
           console.log(err, res);
           pool.end();
         }
       );
-res.sendFile(path.join(__dirname+'/dist/elearning-app/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/elearning-app/index.html'));
 });
 
 app.post('/log', function(req,res) {
@@ -35,7 +35,15 @@ app.post('/log', function(req,res) {
     let body = req.body;
 
     pool.query(
-        "INSERT INTO public.log(id, edad, sexo, tiempocountries, tiempoquiz, aciertos, fallos, tiempo) VALUES ('" + body.id + "','" + body.edad + "','" + body.sexo + "'," + body.tiempoCountries + "," + body.tiempoQuiz + "," + body.respuestasAcertadas + "," + body.respuestasFalladas + ",'" + body.date + "');",
+        "INSERT INTO public.log(id, edad, sexo, tiempocountries, tiempoquiz, aciertos, fallos, tiempo, pistasrestantes) VALUES ('prueba','1','',14,14,14,14,'',2);",
+        (err, res) => {
+          console.log(err, res);
+          pool.end();
+        }
+      );
+
+    pool.query(
+        "INSERT INTO public.log(id, edad, sexo, tiempocountries, tiempoquiz, aciertos, fallos, tiempo, pistasrestantes) VALUES ('" + body.id + "','" + body.edad + "','" + body.sexo + "'," + body.tiempoCountries + "," + body.tiempoQuiz + "," + body.respuestasAcertadas + "," + body.respuestasFalladas + ",'" + body.date + "'," + body.pistasRestantes +");",
         (err, res) => {
             console.log(err, res);
             pool.end();
